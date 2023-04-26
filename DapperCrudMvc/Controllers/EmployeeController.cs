92ca02cfcd6aaeeb2b899506d1267d915d741a3f
Merve <mervebaskan@gtu.edu.tr>
2023-04-26 22:05:58 +0300

    Add project files.

---
+++ DapperCrudMvc/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using DapperCrudMvc.Dapper;
+using DapperCrudMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DapperCrudMvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(DapperORM.ReturnList<EmployeeModel>("EmployeeViewAll"));
+        }
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+            {
+                return View();
+            }
+            else
+            {
+                DynamicParameters param = new DynamicParameters();
+                param.Add("@EmployeeId", id);
+                return View(DapperORM.ReturnList<EmployeeModel>("EmployeeById", param).FirstOrDefault<EmployeeModel>());
+            }
+        }
+        //burda id 0 ise null da döner ya da redirect deriz ya da alert gelsin vs.
+
+        [HttpPost]
+        public ActionResult AddOrEdit(EmployeeModel emp)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@EmployeeId", emp.EmployeeId);
+            param.Add("@Name", emp.Name);
+            param.Add("@Position", emp.Position);
+            param.Add("@Age", emp.Age);
+            param.Add("@Salary", emp.Salary);
+            DapperORM.ExecuteWithoutReturn("EmployeeAddOrEdit", param);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@EmployeeId", id);
+            DapperORM.ExecuteWithoutReturn("EmployeeDeleteById",param);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DapperCrudMvc/Dapper/DapperORM.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DapperCrudMvc.Dapper
+{
+    public static class DapperORM
+    {
+
+        //dapper da önce , db bağlantısı, sql connect, sql query , sql prodecure
+        private static string connectionString = @"Data Source=DESKTOP-T21U4SI\SQLEXPRESS;Initial Catalog=DapperDb;Integrated Security=True;";
+
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param=null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                sqlcon.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public static T ExecuteReturnSalary<T>(string procedureName, DynamicParameters param=null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                return (T)Convert.ChangeType(sqlcon.ExecuteScalar(procedureName, param, commandType: CommandType.StoredProcedure), typeof(T));
+            }
+        }
+
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param=null)
+        {
+            using (SqlConnection sqlcon = new SqlConnection(connectionString))
+            {
+                sqlcon.Open();
+                return sqlcon.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
